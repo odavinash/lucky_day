b3304a185cec7fc85d0e5fb68f25410d6ce27896
avinashod <avinashod@jewelstud.io>
2019-04-24 20:33:40 +0530

    Fix leaderboardAPI

--- lucky_day/lucky_day/utils.py
+++ lucky_day/lucky_day/utils.py
@@ -1,5 +1,6 @@
-from lucky_dayapp.models import Profile
+from lucky_dayapp.models import Profile, LeaderBoard
 
 
 def create_profile(user):
 	Profile.objects.create(user_id = user)
+	LeaderBoard.objects.create(user_id = user, rank_no=0)
