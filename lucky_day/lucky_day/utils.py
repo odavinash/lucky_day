c2e6f725e6f47bc0b6c15b463afc4b386bf11843
avinashod <avinashod@jewelstud.io>
2019-06-14 16:35:16 +0530

    fix issue on social login

--- lucky_day/lucky_day/utils.py
+++ lucky_day/lucky_day/utils.py
@@ -2,5 +2,5 @@ from lucky_dayapp.models import Profile, LeaderBoard
 
 
 def create_profile(user):
-	Profile.objects.create(user_id = user, coin = 0, cash = 0)
-	LeaderBoard.objects.create(user_id = user, rank_no=0)
+	Profile.objects.get_or_create(user_id = user)
+	LeaderBoard.objects.get_or_create(user_id = user, rank_no=0)
